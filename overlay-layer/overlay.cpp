4331313fca00f3d015d7293af6aada48a2510a64
jackun <jack.un@gmail.com>
2020-01-26 15:39:14 +0200

    Removed graph background, plotlines color to green

--- overlay-layer/overlay.cpp
+++ overlay-layer/overlay.cpp
@@ -1183,6 +1183,7 @@ static void compute_swapchain_display(struct swapchain_data *data)
          if (s == OVERLAY_PARAM_ENABLED_gpu_timing)
             data->time_dividor = 1000000.0f;
 
+         ImGui::PushStyleColor(ImGuiCol_FrameBg, ImVec4(0.0f, 0.0f, 0.0f, 0.0f));
          if (s == OVERLAY_PARAM_ENABLED_frame_timing ||
             s == OVERLAY_PARAM_ENABLED_acquire_timing ||
             s == OVERLAY_PARAM_ENABLED_present_timing ||
@@ -1209,6 +1210,7 @@ static void compute_swapchain_display(struct swapchain_data *data)
             //             get_stat(data, ARRAY_SIZE(data->frames_stats) - 1),
             //             data->stats_min.stats[s], data->stats_max.stats[s]);
          }
+         ImGui::PopStyleColor();
       }
       data->window_size = ImVec2(data->window_size.x, ImGui::GetCursorPosY() + 10.0f);
    }
@@ -1887,6 +1889,10 @@ static void setup_swapchain_data(struct swapchain_data *data,
 
    ImGui::GetIO().IniFilename = NULL;
    ImGui::GetIO().DisplaySize = ImVec2((float)data->width, (float)data->height);
+
+   ImGuiStyle& style = ImGui::GetStyle();
+   //style.Colors[ImGuiCol_FrameBg]   = ImVec4(0.0f, 0.0f, 0.0f, 0.00f); // Setting temporarily with PushStyleColor()
+   style.Colors[ImGuiCol_PlotLines] = ImVec4(0.0f, 1.0f, 0.0f, 1.00f);
 
    struct device_data *device_data = data->device;
 
